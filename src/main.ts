1b2e8eb3fa9aa0b33f83a8b532dc524a036ad9b3
Briana Roberts <brianaroberts@Brianas-MacBook-Air.local>
2025-08-21 15:39:35 -0700

    test push

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 class DeprecatedTask {
 	public async run() {
 		// Code Index deprecated, returning error on build.
+		// Test
 		console.log(`As of January 2024, the RichCodeNavIndexer task is being deprecated. Please remove this task from your CI.`);
 	}
 }
